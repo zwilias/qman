58e8a716bf9139ea7b0d851f87f92379dc593577
Ilias <ilias@idlemonkeys.net>
2015-08-02 21:23:56 +0200

    Add QMan, the Queue Manager

---
+++ src/QMan.php
@@ -0,0 +1,124 @@
+<?php
+
+
+namespace QMan;
+
+
+use Beanie\Beanie;
+use Beanie\Exception\AbstractServerException;
+use Beanie\Producer;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+
+class QMan implements LoggerAwareInterface
+{
+    use LoggerAwareTrait;
+
+    /** @var Producer */
+    protected $producer;
+
+    /** @var CommandSerializer */
+    protected $serializer;
+
+    /** @var bool */
+    protected $fallbackEnabled = false;
+
+    /**
+     * @param Producer $producer
+     * @param CommandSerializer $serializer
+     * @param LoggerInterface|null $logger
+     */
+    public function __construct(
+        Producer $producer, CommandSerializer $serializer = null, LoggerInterface $logger = null
+    ) {
+        $this->producer = $producer;
+        $this->logger = $logger ?: new NullLogger();
+        $this->serializer = $serializer ?: new GenericCommandSerializer();
+    }
+
+    /**
+     * @param Command $command
+     * @param int $priority
+     * @param int $delay
+     * @param int $timeToRun
+     */
+    public function queue(
+        Command $command,
+        $priority = Beanie::DEFAULT_PRIORITY,
+        $delay = Beanie::DEFAULT_DELAY,
+        $timeToRun = Beanie::DEFAULT_TIME_TO_RUN
+    ) {
+        try {
+            $this->producer->put(
+                $this->serializer->serialize($command),
+                $priority,
+                $delay,
+                $timeToRun
+            );
+        } catch (AbstractServerException $serverException) {
+            $this->handlePutFailure($serverException, $command);
+        }
+    }
+
+    /**
+     * @param AbstractServerException $exception
+     * @param Command $command
+     * @throws AbstractServerException
+     */
+    protected function handlePutFailure(AbstractServerException $exception, Command $command)
+    {
+        $this->logger->alert('Failed to queue command due to server-exception', [
+            'exception' => $exception,
+            'producer' => $this->producer,
+            'command' => $command
+        ]);
+
+        if ($this->fallbackEnabled === true) {
+            $command->execute();
+        } else {
+            throw $exception;
+        }
+    }
+
+    public function enableFallback()
+    {
+        $this->fallbackEnabled = true;
+    }
+
+    public function queueClosure(
+        \Closure $closure,
+         $priority = Beanie::DEFAULT_PRIORITY,
+         $delay = Beanie::DEFAULT_DELAY,
+         $timeToRun = Beanie::DEFAULT_TIME_TO_RUN
+    ) {
+        $closureCommand = new ClosureCommand();
+        $closureCommand->setClosure($closure);
+
+        $this->queue($closureCommand, $priority, $delay, $timeToRun);
+    }
+
+    /**
+     * @return CommandSerializer
+     */
+    public function getSerializer()
+    {
+        return $this->serializer;
+    }
+
+    /**
+     * @param string[] $servers
+     * @param CommandSerializer $serializer
+     * @param LoggerInterface $logger
+     * @return static
+     */
+    public static function create(array $servers, CommandSerializer $serializer = null, LoggerInterface $logger = null)
+    {
+        return new static(
+            Beanie::pool($servers)->producer(),
+            $serializer,
+            $logger
+        );
+    }
+}
---
+++ tests/QMan/QManTest.php
@@ -0,0 +1,221 @@
+<?php
+
+
+namespace QMan;
+
+
+use Beanie\Exception\AbstractServerException;
+use Beanie\Producer;
+
+/**
+ * Class QManTest
+ * @package QMan
+ * @covers \QMan\Qman
+ */
+class QManTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var \PHPUnit_Framework_MockObject_MockObject|Producer */
+    protected $producerMock;
+
+    /** @var \PHPUnit_Framework_MockObject_MockObject|CommandSerializer */
+    protected $serializerMock;
+
+    /** @var QMan */
+    protected $qMan;
+
+    /** @var \PHPUnit_Framework_MockObject_MockObject|Command */
+    protected $commandMock;
+
+    public function setUp()
+    {
+        $this->producerMock = $this
+            ->getMockBuilder(Producer::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['put'])
+            ->getMock();
+
+        $this->serializerMock = $this
+            ->getMockBuilder(CommandSerializer::class)
+            ->setMethods(['serialize'])
+            ->getMockForAbstractClass();
+
+        $this->commandMock = $this
+            ->getMockBuilder(Command::class)
+            ->setMethods(['getType', 'getData', 'execute'])
+            ->getMockForAbstractClass();
+
+        $this->qMan = new QMan($this->producerMock, $this->serializerMock);
+    }
+
+    public function testGetSerializer_returnsCurrentSerializer()
+    {
+        $this->assertSame($this->serializerMock, $this->qMan->getSerializer());
+    }
+
+    /**
+     * @param array $params
+     * @dataProvider queueParamsProvider
+     */
+    public function testQueue_queues(array $params)
+    {
+        $testData = 'test';
+
+        $this->serializerMock
+            ->expects($this->once())
+            ->method('serialize')
+            ->with($this->commandMock)
+            ->willReturn($testData);
+
+        $invocationMocker = $this->producerMock
+            ->expects($this->once())
+            ->method('put');
+
+        call_user_func_array([$invocationMocker, 'with'], array_merge([$testData], $params));
+
+
+        call_user_func_array([$this->qMan, 'queue'], array_merge([$this->commandMock], $params));
+    }
+
+    /**
+     * @expectedException \Beanie\Exception\AbstractServerException
+     * @dataProvider queueParamsProvider
+     * @param array $params
+     */
+    public function testQueue_noFallback_exceptionThrown(array $params)
+    {
+        $testData = 'test';
+
+        $this->serializerMock
+            ->expects($this->once())
+            ->method('serialize')
+            ->with($this->commandMock)
+            ->willReturn($testData);
+
+        $invocationMocker = $this->producerMock
+            ->expects($this->once())
+            ->method('put');
+
+        $serverExceptionMock = $this
+            ->getMockBuilder(AbstractServerException::class)
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+        $invocationMocker = call_user_func_array([$invocationMocker, 'with'], array_merge([$testData], $params));
+        $invocationMocker->willThrowException($serverExceptionMock);
+
+
+        call_user_func_array([$this->qMan, 'queue'], array_merge([$this->commandMock], $params));
+    }
+
+
+    /**
+     * @expectedException \RuntimeException
+     * @dataProvider queueParamsProvider
+     * @param array $params
+     */
+    public function testQueue_fallbackEnabled_exceptionThrown(array $params)
+    {
+        $testData = 'test';
+
+        $this->qMan->enableFallback();
+
+        $this->serializerMock
+            ->expects($this->once())
+            ->method('serialize')
+            ->with($this->commandMock)
+            ->willReturn($testData);
+
+
+        $invocationMocker = $this->producerMock
+            ->expects($this->once())
+            ->method('put');
+
+        $invocationMocker = call_user_func_array([$invocationMocker, 'with'], array_merge([$testData], $params));
+        $invocationMocker->willThrowException(new \RuntimeException());
+
+
+        call_user_func_array([$this->qMan, 'queue'], array_merge([$this->commandMock], $params));
+    }
+
+    /**
+     * @dataProvider queueParamsProvider
+     * @param array $params
+     */
+    public function testQueue_fallbackEnabled_serverExceptionMeansTryAgain(array $params)
+    {
+        $testData = 'test';
+
+        $this->qMan->enableFallback();
+
+        $this->commandMock
+            ->expects($this->once())
+            ->method('execute');
+
+        $this->serializerMock
+            ->expects($this->once())
+            ->method('serialize')
+            ->with($this->commandMock)
+            ->willReturn($testData);
+
+        $serverExceptionMock = $this
+            ->getMockBuilder(AbstractServerException::class)
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+        $invocationMocker = $this->producerMock
+            ->expects($this->once())
+            ->method('put');
+
+        $invocationMocker = call_user_func_array([$invocationMocker, 'with'], array_merge([$testData], $params));
+        $invocationMocker->willThrowException($serverExceptionMock);
+
+
+        call_user_func_array([$this->qMan, 'queue'], array_merge([$this->commandMock], $params));
+    }
+
+    /**
+     * @param array $params
+     * @dataProvider queueParamsProvider
+     */
+    public function testQueueClosure_queues(array $params)
+    {
+        $closure = function () {
+            $this->fail('Should not be executed');
+        };
+
+        $testData = 'test';
+
+        $this->serializerMock
+            ->expects($this->once())
+            ->method('serialize')
+            ->with($this->isInstanceOf(ClosureCommand::class))
+            ->willReturn($testData);
+
+        $invocationMocker = $this->producerMock
+            ->expects($this->once())
+            ->method('put');
+
+        call_user_func_array([$invocationMocker, 'with'], array_merge([$testData], $params));
+
+
+        call_user_func_array([$this->qMan, 'queueClosure'], array_merge([$closure], $params));
+    }
+
+    public function testStaticCreate_createsFromListOfServerNames()
+    {
+        $qMan = QMan::create(['localhost:11300', 'localhost:11301']);
+
+
+        $this->assertInstanceOf(QMan::class, $qMan);
+    }
+
+    public function queueParamsProvider()
+    {
+        return [
+            [[]],
+            [[123]],
+            [[321, 44]],
+            [[333, 31, 523]]
+        ];
+    }
+}
