493168eaef7f7dc8b23d38277b62dcc1a0825ccc
Ilias <ilias@idlemonkeys.net>
2015-08-02 19:50:33 +0200

    Test AbstractCommandSerializer

---
+++ tests/QMan/AbstractCommandSerializerTest.php
@@ -0,0 +1,73 @@
+<?php
+
+
+namespace QMan;
+
+
+/**
+ * Class AbstractCommandSerializerTest
+ * @package QMan
+ * @covers \QMan\AbstractCommandSerializer
+ */
+class AbstractCommandSerializerTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var \PHPUnit_Framework_MockObject_MockObject|AbstractCommandSerializer */
+    private $serializer;
+
+    public function setUp()
+    {
+        $this->serializer = $this
+            ->getMockBuilder(AbstractCommandSerializer::class)
+            ->setMethods(['createCommand'])
+            ->getMockForAbstractClass();
+    }
+
+    public function testSerialize_createsJSONSerializedArray()
+    {
+        $testType = 'test';
+        $testData = 'testdata';
+
+        $expected = json_encode([
+            'type' => $testType,
+            'data' => $testData
+        ]);
+
+        /** @var \PHPUnit_Framework_MockObject_MockObject|Command $commandMock */
+        $commandMock = $this
+            ->getMockBuilder(Command::class)
+            ->setMethods(['getType', 'getData'])
+            ->getMockForAbstractClass();
+
+        $commandMock
+            ->expects($this->once())
+            ->method('getType')
+            ->willReturn($testType);
+
+        $commandMock
+            ->expects($this->once())
+            ->method('getData')
+            ->willReturn($testData);
+
+
+        $this->assertEquals($expected, $this->serializer->serialize($commandMock));
+    }
+
+    public function testUnserialize_createsCommandWithTypeAndData()
+    {
+        $expectedType = 'test';
+        $expectedData = 'testdata';
+
+        $testData = json_encode([
+            'type' => $expectedType,
+            'data' => $expectedData
+        ]);
+
+        $this->serializer
+            ->expects($this->once())
+            ->method('createCommand')
+            ->with($expectedType, $expectedData);
+
+
+        $this->serializer->unserialize($testData);
+    }
+}
